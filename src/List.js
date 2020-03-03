06187a2e1c881786f5e99994ed906273e7e8aa1d
solcodeguy <smeighn@gmail.com>
2020-03-03 16:08:11 -0500

    Update all files

--- src/List.js
+++ src/List.js
@@ -8,22 +8,30 @@ export default function List(props) {
         <
         header className = 'List-header' >
         <
-        h2 > { props.header } < /h2> <
-        /header> <
+        h2 > { props.header } < /h2> < /
+        header > <
         div className = 'List-cards' > {
             props.cards.map((card) =>
                 <
                 Card key = { card.id }
+                id = { card.id }
                 title = { card.title }
                 content = { card.content }
+                onClickDelete = { props.onClickDelete }
                 />
             )
         } <
         button type = 'button'
-        className = 'List-add-button' >
+        className = 'List-add-button'
+        onClick = {
+            () => props.onClickAdd(props.id) } >
         +Add Random Card <
-        /button> <
-        /div> <
+        /button> < /
+        div > <
         /section>
     )
+}
+
+List.defaultProps = {
+    onClickAdd: () => {},
 }
